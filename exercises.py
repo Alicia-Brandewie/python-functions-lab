de0425fa580738ce80d6d7ec90b8142fcecddb23
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-09-03 20:27:12 -0500

    one-three complete

--- exercises.py
+++ exercises.py
@@ -25,7 +25,25 @@ print('Exercise 1:', calculate_area_triangle(7, 3))
 #
 # Define your function and call it to see the result.
 def simple_interest(principal, rate, time):
-    intrest =(principal * rate * time) / 100
+    intrest = (principal * rate * time) / 100
     return intrest
 
 print('Exercise 2:', simple_interest(1500, 3.5, 5))
+
+# Exercise 3: Apply a Discount
+#
+# Write a function named `apply_discount` that takes a product's price and a discount percentage (from 0 to 100).
+# Return the new price after applying the discount.
+#
+# Examples:
+# apply_discount(100, 25) should return 75.
+# apply_discount(80, 10) should return 72.
+#
+# Define your function and call it to display the discounted price.
+def apply_discount(price, discount):
+    new_price = price - (discount/100 * price)
+    return new_price
+
+
+print('Exercise 3:', apply_discount(100, 25))
+print('Exercise 3:', apply_discount(80, 10))
